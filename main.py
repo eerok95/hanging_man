e645bc3449f73ef4b2235bbb44b8b5350fa10c10
eerok95 <eero.e.keinanen@gmail.com>
2026-01-21 12:31:10 +0200

    added game screen and a forever-loop structure to main

--- main.py
+++ main.py
@@ -1,6 +1,29 @@
+from constants import *
+import sys
 import pygame
 def main():
     print("Hello from hanging-man!")
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+            
+        screen.fill((0, 0, 0))
+        for up in updatable:
+            up.update(dt)
+        for dr in drawable:
+            dr.draw(screen)
+        pygame.display.flip()
+        dt = clock.tick(60) / 1000  # Convert milliseconds to seconds
 
 
 if __name__ == "__main__":
